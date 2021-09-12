c804ceb56b1a68366dfd217d1296a052d313a243
Izsán Luca <izsan.luca04@gmail.com>
2021-09-12 14:13:41 +0200

    add base file structure

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function random()
 {
-    let i = Math.floor(Math.random() * 9) + 1;
+    let i = Math.floor(Math.random() * 19) + 1;
     let hely = document.getElementById("bekahelye");
     hely.src = "r" + i + ".jpg"; 
 }
 
 function meme()
 {
-    let i = Math.floor(Math.random() * 9) + 1;
+    let i = Math.floor(Math.random() * 19) + 1;
     let hely = document.getElementById("bekahelye");
     hely.src = "meme" + i + ".jpg"; 
 }
 
 function jf()
 {
-    let i = Math.floor(Math.random() * 9) + 1;
+    let i = Math.floor(Math.random() * 19) + 1;
     let hely = document.getElementById("bekahelye");
     hely.src = "jf" + i + ".jpg"; 
 }
